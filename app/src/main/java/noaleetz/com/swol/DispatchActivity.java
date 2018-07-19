0fc64770b86fd737b14c1ab91078ed3dbd52052e
Omari Matthews <omatthews@fb.com>
2018-07-19 16:06:12 -0700

    temporary commit--switching to master

--- app/src/main/java/noaleetz/com/swol/DispatchActivity.java
+++ app/src/main/java/noaleetz/com/swol/DispatchActivity.java
@@ -3,4 +3,9 @@ package noaleetz.com.swol;
 import com.parse.ui.login.ParseLoginDispatchActivity;
 
 public class DispatchActivity extends ParseLoginDispatchActivity {
+
+    @Override
+    protected Class<?> getTargetClass() {
+        return MainActivity.class;
+    }
 }
