425a7dcac06dc1e344c5d51bf8e454e41e26e75f
Noa Schwartz <noaleetz@fb.com>
2018-07-24 16:43:08 -0700

    onClick in map now leads to corresponding detail screen

--- app/src/main/java/noaleetz/com/swol/MainActivity.java
+++ app/src/main/java/noaleetz/com/swol/MainActivity.java
@@ -352,7 +352,6 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle.onConfigurationChanged(newConfig);
     }
 
-<<<<<<< HEAD
 
     public void changeToDetailFragment(Workout workout) {
         //TODO that
@@ -375,13 +374,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-=======
     public boolean isFacebookUser(ParseUser user) {
         if (user.get("authData") == null) return false;
         JSONObject authData = user.getJSONObject("authData");
         return authData.has("facebook");
     }
->>>>>>> master
 
 
 }
